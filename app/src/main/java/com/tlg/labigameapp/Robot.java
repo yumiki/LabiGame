805a12fa4a7410a94311f08c1623348851242815
yumiki <yumiki_lords@hotmail.fr>
2016-10-20 15:33:16 +0200

    Create Robot.java
    Add Robot class

---
+++ app/src/main/java/com/tlg/labigameapp/Robot.java
@@ -0,0 +1,20 @@
+package com.tlg.labigameapp;
+
+public class Robot {
+
+    Point position;
+
+
+
+    public void deplacerHaut(){
+
+    this.position=this.y+1;
+    }
+
+
+    public void deplacerBas(){
+
+
+    }
+
+}
