0a1479e1ef3a2c0be8ec383f0cc364a1fc5ddec8
yumiki <novar.ludovic@gmail.com>
2016-10-20 16:35:30 +0200

    Generer labi en cour

--- app/src/main/java/com/tlg/labigameapp/Labyrinthe.java
+++ app/src/main/java/com/tlg/labigameapp/Labyrinthe.java
@@ -5,10 +5,17 @@ public class Labyrinthe {
 
     Point depart;
     Point arrivee;
-    boolean Case;
+    boolean[][] cases;
 
 
     public void generer(){
+
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++) {
+                cases[i][j]=false;
+            }
+        }
+
 
     }
 
